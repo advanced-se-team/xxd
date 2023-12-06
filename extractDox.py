65ee2ed4ff64b76dd130057d461639108b7d6e28
xxd-pwn <55626397+xxd-pwn@users.noreply.github.com>
2023-12-06 19:52:08 +0800

    Add files via upload
    按章节解析内容

--- extractDox.py
+++ extractDox.py
@@ -3,7 +3,10 @@ from time import sleep
 
 import convertapi
 from docx import Document
+from fitz import fitz
 from loguru import logger
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer, LTChar, LAParams
 from docx.oxml import OxmlElement
 
 def extract_text(dox_dir: str):
@@ -11,9 +14,22 @@ def extract_text(dox_dir: str):
     doc = Document(dox_dir)
     navigationBra = 1
     authorContent = 0
-    abstract = 0
     paperDict = {}  # 保存的内容为'contextType' = 'content'
     pre_paragraph = doc.paragraphs[0]
+    contentType2 = 'No classification'
+
+    #取出目录
+    pdf = fitz.open(dox_dir.replace('docx', 'pdf'))
+    toc = pdf.get_toc()  # 获取pdf目录
+    catalogue = []
+    for t in toc:
+        if t[0] == 1:
+        # print(t)
+            catalogue.append(t[1].lower())
+    # print(catalogue)
+
+    abstract = 0 if 'abstract' in catalogue else 1
+
     # 遍历文档中的段落并打印文本内容
     for paragraph in doc.paragraphs:
         tmp = paragraph.text.replace(" ", "").replace("\n", "")
@@ -58,7 +74,6 @@ def extract_text(dox_dir: str):
         elif alignment == 1 and authorContent == 1:
             contentType = 'Authors Information'
             content = paragraph.text
-            abstract = 1
             if not contentType in paperDict:
                 paperDict[contentType] = paragraph.text
             else:
@@ -74,15 +89,18 @@ def extract_text(dox_dir: str):
                 paperDict[contentType] += '\n' + paragraph.text
 
         else:
+            authorContent = 0
             abstract = 0
-            contentType = 'Main Body'
-            if not contentType in paperDict:
-                paperDict[contentType] = paragraph.text
+            if paragraph.text.lower() in catalogue:
+                contentType2 = paragraph.text
+            # else:
+            #     logger.debug(f'paragraph.text = \n{paragraph.text}\ncatalogue2 = \n{catalogue}')
+            if not contentType2 in paperDict:
+                paperDict[contentType2] = paragraph.text
             else:
-                paperDict[contentType] += '\n' + paragraph.text
+                paperDict[contentType2] += '\n' + paragraph.text
 
 
-        pre_paragraph = paragraph
     return paperDict
 
 def pdf_to_docx(PDFDir):
@@ -93,9 +111,9 @@ def pdf_to_docx(PDFDir):
 
 if __name__ == '__main__':
 
-    PDFDir = 'Kingsbury and Alvaro - 2020 - Elle inferring isolation anomalies from experimen(1).pdf'
+    PDFDir = 'icse2023a.pdf'
     DOCXDir = PDFDir.replace(".pdf", ".docx")
-
+    #
     # 将pdf转为word，如果该word已经存在则不执行
     if not (os.path.exists(PDFDir.replace(".pdf", ".docx"))):
         logger.info('Convertion starts...')
@@ -111,46 +129,85 @@ if __name__ == '__main__':
         print(value)
         sleep(1)
 
-    # 打印Word文档里的每段文字属性
-    doc = Document(DOCXDir)
+    # # 打印Word文档里的每段文字属性-- 调试语句
+    # doc = Document(DOCXDir)
+    #
+    # # 遍历文档中的段落并打印文本内容
+    # for paragraph in doc.paragraphs:
+    #     tmp = paragraph.text.replace(" ", "").replace("\n", "")
+    #     if not tmp:
+    #         continue
+    #     print(paragraph.text)
+    #     # 获取段落的样式
+    #     style = paragraph.style.name
+    #     print(f"段落样式：{style}")
+    #     for run in paragraph.runs:
+    #         # 获取文本的具体属性
+    #         text = run.text  # 文本内容
+    #         font_name = run.font.name  # 字体名称
+    #         font_size = run.font.size  # 字体大小
+    #         bold = run.font.bold  # 是否加粗
+    #         italic = run.font.italic  # 是否斜体
+    #         underline = run.font.underline  # 是否有下划线
+    #         # 获取段落的对齐方式
+    #         alignment = paragraph.alignment
+    #
+    #         # 根据对齐方式输出文字对齐方式
+    #         if alignment == 0:
+    #             print("左对齐")
+    #         elif alignment == 1:
+    #             print("居中对齐")
+    #         elif alignment == 2:
+    #             print("右对齐")
+    #         elif alignment == 3:
+    #             print("两端对齐")
+    #         else:
+    #             print("未知对齐方式")
+    #         # 打印文本的具体属性
+    #         print(f"文本内容：{text}")
+    #         print(f"字体名称：{font_name}")
+    #         print(f"字体大小：{font_size}")
+    #         print(f"是否加粗：{bold}")
+    #         print(f"是否斜体：{italic}")
+    #         print(f"是否有下划线：{underline}")
+    #         print()
+    #         break
 
-    # 遍历文档中的段落并打印文本内容
-    for paragraph in doc.paragraphs:
-        tmp = paragraph.text.replace(" ", "").replace("\n", "")
-        if not tmp:
-            continue
-        print(paragraph.text)
-        # 获取段落的样式
-        style = paragraph.style.name
-        print(f"段落样式：{style}")
-        for run in paragraph.runs:
-            # 获取文本的具体属性
-            text = run.text  # 文本内容
-            font_name = run.font.name  # 字体名称
-            font_size = run.font.size  # 字体大小
-            bold = run.font.bold  # 是否加粗
-            italic = run.font.italic  # 是否斜体
-            underline = run.font.underline  # 是否有下划线
-            # 获取段落的对齐方式
-            alignment = paragraph.alignment
+    # 你的PDF文件路径
+    path = 'icse2023a.pdf'
 
-            # 根据对齐方式输出文字对齐方式
-            if alignment == 0:
-                print("左对齐")
-            elif alignment == 1:
-                print("居中对齐")
-            elif alignment == 2:
-                print("右对齐")
-            elif alignment == 3:
-                print("两端对齐")
-            else:
-                print("未知对齐方式")
-            # 打印文本的具体属性
-            print(f"文本内容：{text}")
-            print(f"字体名称：{font_name}")
-            print(f"字体大小：{font_size}")
-            print(f"是否加粗：{bold}")
-            print(f"是否斜体：{italic}")
-            print(f"是否有下划线：{underline}")
-            print()
-            break
+    # 用于存储提取的数据
+    extracted_data = []
+
+    # # 提取PDF文档中每个段落的字体大小和文本内容
+    # for page_layout in extract_pages(path):
+    #     for element in page_layout:
+    #         if isinstance(element, LTTextContainer):
+    #             print(element.get_text())
+    #             for text_line in element:
+    #                 for character in text_line:
+    #                     if isinstance(character, LTChar):
+                            # # 获取字体大小、字体名称、文本内容
+                            # font_size = character.size
+                            # font_name = character.fontname
+                            # text = character.get_text()
+                            # # 获取位置信息
+                            # x0, y0, x1, y1 = character.bbox
+                            # # 获取字距和行距
+                            # width = character.width
+                            # adv = character.adv
+                            # # 打印文本属性
+                            # print(f"字体大小: {font_size}, 字体名称: {font_name}, 文本内容: {text}")
+                            # print(f"位置信息: 左上角({x0}, {y0}), 右下角({x1}, {y1})")
+                            # print(f"字距: {width}, 行距: {adv}")
+                            # break
+
+    # doc = fitz.open('icse2023a.pdf')
+    # toc = doc.get_toc()  # 获取pdf目录
+    # catalogue = []
+    # for t in toc:
+    #     if t[0] == 1:
+    #         print(t[1])
+    #         catalogue.append(t[1].lower())
+    #
+    # print(catalogue)
